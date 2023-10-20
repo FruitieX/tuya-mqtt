dded6478efc12b7442b25f2543e0c24ec658ecac
Rasmus Lövegren <rasmus@lovegren.fi>
2023-10-20 18:07:01 +0300

    chore: cleanup unused binding in default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,4 @@
 let
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz );
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   rustStableChannel = (nixpkgs.rustChannels.stable).rust.override {
